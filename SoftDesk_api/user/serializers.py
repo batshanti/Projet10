8efd147aa5331972f50bc18b90bb69f5ff5a13c2
batshanti <teddylouvet@yahoo.fr>
2022-07-28 16:56:42 +0200

    add SignupSerializer

---
+++ SoftDesk_api/user/serializers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+
+class SignupSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'password', 'password2']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+
+            raise serializers.ValidationError(
+                'password and password2 are different'
+            )
+        return data
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- SoftDesk_api/user/views.py
+++ SoftDesk_api/user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from user.serializers import SignupSerializer
 
-# Create your views here.
+
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
